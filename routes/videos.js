2f473650702391785a088a46ae5f13fd2e08cf2c
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-13 16:45:40 -0700

    Update get request for specific video to provide all details for the provided video id

--- routes/videos.js
+++ routes/videos.js
@@ -5,22 +5,21 @@ const videosData = require("../data/video-details.json");
 //Provide list of videos
 router.get("/", (request, response) => {
     console.log("Request received");
-    response
-        .status(200)
-        .send(
-            videosData.map((video) => ({
-                id: video.id,
-                title: video.title,
-                channel: video.channel,
-                image: video.image,
-            }))
-        );
+    response.status(200).send(
+        videosData.map((video) => ({
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image,
+        }))
+    );
 });
 
 //Provide details for selected video
 router.get("/:id", (request, response) => {
     console.log("Request received for specific video");
-    response.status(200).send("Request received for specific video");
+    const videoId = request.params.id;
+    response.status(200).send(videosData.find((video) => video.id === videoId));
 });
 
 module.exports = router;
