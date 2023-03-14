bcaa7f57fc2a7ca6fb24a49846e24bf4a6ce3653
Manjot Virdi <virdi_manjot@hotmail.com>
2023-03-14 15:39:01 -0700

    Add images to public/images; update server.js for middware for cors, json, access to public folder; test use of public folder by updating path for image of first video in json data; update get video list call to use file system; remove .env from gitignore

--- routes/videos.js
+++ routes/videos.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid"); // Unique ID Generator
+const fs = require("fs"); // fileSystem module
 const videosData = require("../data/video-details.json");
+
+//function to read data file
+function readFile(file, callback) {
+    fs.readFile(file, "utf8", callback);
+}
+
+//function to write to data file
+function writeFile(file, data, callback) {
+    fs.writeFile(file, JSON.stringify(data), callback);
+}
 
 //Provide list of videos
 router.get("/", (request, response) => {
-    console.log("Request received");
-    response.status(200).send(
-        videosData.map((video) => ({
-            id: video.id,
-            title: video.title,
-            channel: video.channel,
-            image: video.image,
-        }))
-    );
+    //Read file
+    readFile("./data/video-details.json", (err, data) => {
+        //Return error if read file failed
+        if (err) {
+            return response.send(err); //see what http error code to send***
+        }
+        //If no error, parse data from json file and send selected details
+        console.log("Request received for list");
+        response.status(200).json(
+            JSON.parse(data).map((video) => ({
+                id: video.id,
+                title: video.title,
+                channel: video.channel,
+                image: video.image,
+            }))
+        );
+    });
 });
 
 //Provide details for selected video
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const videoRoutes = require("./routes/videos");
 
 const port = 5001;
 
+// Middleware
+app.use(cors()); //allows sharing data / api calls between apps on different servers
+app.use(express.static("public")); //allows access to the public folder, where video images are stored
+app.use(express.json()); //allows access to incoming data posted to the server as part of the req.body
+
+// Routes
 app.use("/videos", videoRoutes);
 
 app.listen(port, () => {
